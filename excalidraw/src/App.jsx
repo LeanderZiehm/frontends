96042ee49cee5e130fb7cf13067cca7d74906b01
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2026-01-06 16:42:28 +0100

    feat: add excalidraw frontend

---
+++ excalidraw/src/App.jsx
@@ -0,0 +1,14 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/index.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ height: "100vh", padding: "0" }}>
+        <Excalidraw />
+      </div>
+    </>
+  );
+}
+
+export default App
